579f1fe5f6003cbee0be9a54e033c6b707c8964b
Zaw <qwasar@zaw.tech>
2019-12-04 07:42:39 -0800

    first

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace PNG2JPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var folder = @"/Users/mo/Downloads";
+            int count = 0;
+
+            foreach (string file in Directory.GetFiles(folder))
+            {
+                string ext = Path.GetExtension(file).ToLower();
+                if (ext == ".png")
+                {
+                    Console.WriteLine(file);
+                    string name = Path.GetFileNameWithoutExtension(file);
+                    string path = Path.GetDirectoryName(file);
+
+                    Image png = Image.FromFile(file);
+                    
+                    png.Save(path + @"/" + name + ".jpg", ImageFormat.Jpeg);
+                    png.Dispose();
+
+                    count++;
+                    File.Delete(file);
+
+                }
+            }
+
+            Console.WriteLine("{0} file(s) converted.", count);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
